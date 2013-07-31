9bd8af8baba1f941e1a4bc7cf57d970cacdee661
Pierre Radermecker <pradermecker@yahoo.ca>
2013-07-31 20:48:37 +0200

    Async server works fine with the proxy function since 0.5

--- examples/Haskell/asyncsrv.hs
+++ examples/Haskell/asyncsrv.hs
@@ -1,29 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import System.ZMQ3.Monadic
-import ZHelpers (setId)
+import ZHelpers (setRandomIdentity)
 import Control.Concurrent (threadDelay)
 import Data.ByteString.Char8 (pack, unpack)
-import Control.Monad (forever, forM_, when, unless, replicateM_)
+import Control.Monad (forever, forM_, when, replicateM_)
 import System.Random (newStdGen, randomR, StdGen)
 
-clientTask :: ZMQ z ()
-clientTask = do
+clientTask :: String -> ZMQ z ()
+clientTask ident = do
     client <- socket Dealer
-    setId client
+    setRandomIdentity client
     connect client "tcp://localhost:5570"
-    forever $ do
-        forM_ [0..100] $ \centitick -> do
-            [evts] <- poll 10 [Sock client [In] Nothing]
-            when (In `elem` evts) $ do
-                receive client >>= \msg -> liftIO $ putStrLn $ unwords ["Client received", (unpack msg), "back from worker"]
-        liftIO $ putStrLn "Req Sent"
-        send client [] (pack "request")
+    forM_ [1..] $ \i -> do -- forever
+        -- tick one per second, pulling in arriving messages
+        forM_ [0..100] $ \_ -> do
+            [evts] <- poll 10 [Sock client [In] Nothing] -- timeout of 10 ms
+            when (In `elem` evts) $
+                receive client >>= \msg -> liftIO $ putStrLn $ unwords ["Client", ident, "has received back from worker its msg \"", (unpack msg), "\""]
+        send client [] (pack $ unwords ["Client", ident, "sends request", show i])
 
-        
+            
 serverTask :: ZMQ z ()
 serverTask =  do
-
     frontend <- socket Router
     bind frontend "tcp://*:5570"
     backend <- socket Dealer
@@ -31,21 +31,7 @@ serverTask =  do
     
     replicateM_ 5 $ async serverWorker
 
-    -- proxy does not work here ?
-    forever $ pollEvt frontend backend
-
-    where
-        pollEvt frontend backend = do 
-            [evtsF, evtsB] <- poll (-1) [Sock frontend [In] Nothing, Sock backend [In] Nothing] 
-            
-            when (In `elem` evtsF) $ do
-                receive frontend >>= send backend [SendMore]
-                receive frontend >>= send backend []
-                            
-            unless (In `elem` evtsB) $ do
-                receive backend >>= send frontend  [SendMore]
-                receive backend >>= send frontend []
-
+    proxy frontend backend Nothing
 
 
 serverWorker :: ZMQ z ()
@@ -56,20 +42,21 @@ serverWorker = do
     forever $ do
         _id <- receive worker
         msg <- receive worker
-        liftIO $ putStrLn "Worker Received Data"
         gen <- liftIO $ newStdGen
-        let (val, _) = randomR (0,5) gen :: (Int, StdGen)
-        forM_ [1..val] $ \i -> do
-            liftIO $ threadDelay $ 1 * 1000 * 1000
+        let (val, gen') = randomR (0,4) gen :: (Int, StdGen)
+            (r, _)      = randomR (1, 1000) gen' :: (Int, StdGen)
+        -- send back the received msg to client 0 to 4 times max
+        forM_ [0..val] $ \_ -> do
+            liftIO $ threadDelay $ r * 1000
             send worker [SendMore] _id
             send worker [] msg
 
 main :: IO ()
 main = 
     runZMQ $ do
-        async clientTask
-        async clientTask
-        async clientTask
+        async $ clientTask "A"
+        async $ clientTask "B"
+        async $ clientTask "C"
         async serverTask
         
-        liftIO $ threadDelay $ 5 * 1000 * 1000
+        liftIO $ threadDelay $ 10 * 1000 * 1000
