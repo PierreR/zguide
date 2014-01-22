bd189b4b2fd9c66f7d7f9f5673ccb29ae35b586a
PierreR <pierrer@pi3r.be>
2014-01-22 13:28:10 +0100

    Guard the client against wrong input

--- examples/Haskell/wuclient.hs
+++ examples/Haskell/wuclient.hs
@@ -3,41 +3,45 @@
 -- Weather broadcast server in Haskell
 -- Binds SUB socket to tcp://localhost:5556
 -- Collects weather updates and finds avg temp in zipcode
--- 
 
 module Main where
 
-import System.ZMQ4.Monadic
-import Control.Monad (replicateM)
 import Control.Applicative((<$>), (<*>))
-import Data.ByteString.Char8 (unpack, pack)
-import Text.Printf
 import qualified Control.Foldl as L
+import Control.Monad (replicateM)
+import qualified Data.ByteString.Char8 as BC
+import Data.Maybe (mapMaybe)
+import Text.Printf
+import Safe (readMay)
+import System.ZMQ4.Monadic
 
--- | Average uses applicative with `Control.Foldl` in order to traverse the list only once.
+zipCode :: BC.ByteString
+zipCode = "10001" -- NYC
+
+-- traverse the list only once thanks to `Control.Foldl` applicatives.
 average :: L.Fold Double Double
 average = (/) <$> L.sum <*> L.genericLength
 
-mapped :: (a -> b) -> L.Fold b r -> L.Fold a r
-mapped f (L.Fold step begin done) = L.Fold step' begin done
+-- premap is part of Foldl since 1.0.2
+premap :: (a -> b) -> L.Fold b r -> L.Fold a r
+premap f (L.Fold step begin done) = L.Fold step' begin done
   where
     step' x = step x . f
 
 main :: IO ()
 main =
-  runZMQ $ do
-    subscriber <- socket Sub
-    connect subscriber "tcp://localhost:5556"
-    -- Subscribe for NY City zipcode
-    subscribe subscriber (pack "10001")
-        
-    records <- replicateM 5 $ do
-      update <- receive subscriber
-      let [_, temp, hum] = map read $ words $ unpack update
-      return (temp, hum)
+    runZMQ $ do
+        subscriber <- socket Sub
+        connect subscriber "ipc://weather.ipc"
+        subscribe subscriber zipCode
 
-    liftIO $ printf "NY City: avg temperature of %.1f°C and avg humidity of %.1f%%\n" (avgTemp records) (avgHum records) 
+        -- process 100 updates
+        records <- replicateM 50 $ do
+            update <- receive subscriber
+            let [_, temp, hum] = mapMaybe readMay $ words (BC.unpack update)
+            return (temp, hum)
+        liftIO $ printf "NY City: avg temp of %.1f°C and avg hum of %.1f%%\n" (avgTemp records) (avgHum records)
 
-    where
-       avgTemp = L.fold (mapped fst average) 
-       avgHum  = L.fold (mapped snd average) 
+        where
+           avgTemp = L.fold (premap fst average)
+           avgHum  = L.fold (premap snd average)
